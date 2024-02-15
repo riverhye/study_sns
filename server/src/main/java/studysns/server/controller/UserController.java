f78aba1e1fd28b034c969366363fd025cb6a89c2
명길식 <myeong-gilsig@MBook.local>
2024-02-16 00:10:54 +0900

    Feat: 회원 가입 창 접근 가능

--- server/src/main/java/studysns/server/controller/UserController.java
+++ server/src/main/java/studysns/server/controller/UserController.java
@@ -1,4 +1,15 @@
 package studysns.server.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
 public class UserController {
+
+    @GetMapping("/signup")
+    public String getSignIn(){
+        return "signup";
+    }
 }
