cf6addb1fd2c48c15537f48d0d2a55e819f3fc25
riverhye <uhzigani@gmail.com>
2024-02-15 15:15:38 +0900

    Design: 헤더

---
+++ client/components/header/HeaderLeft.tsx
@@ -0,0 +1,11 @@
+import Category from './Category';
+
+const HeaderLeft = () => {
+  return (
+    <div className="h-screen w-1/5 border-2 min-w-72 p-9">
+      <Category />
+    </div>
+  );
+};
+
+export default HeaderLeft;
