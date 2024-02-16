48741c2fd1c078f071c2a2437cc693bab0fa94e9
명길식 <myeong-gilsig@MBook.local>
2024-02-16 21:03:16 +0900

    Feat: 회원 가입 관련 기능 코드 작성

--- server/src/main/java/studysns/server/controller/UserController.java
+++ server/src/main/java/studysns/server/controller/UserController.java
@@ -1,15 +1,47 @@
 package studysns.server.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import studysns.server.dto.UserDTO;
+import studysns.server.entity.UserEntity;
+import studysns.server.service.UserService;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    UserService userService;
+
     @GetMapping("/signup")
     public String getSignIn(){
         return "GET: user";
+    }
+
+    @PostMapping("/signup/process")
+    public ResponseEntity<?> registerUser(@RequestBody UserDTO userDTO) {
+        try {
+            UserEntity userEntity = UserEntity.builder()
+                    .email(userDTO.getEmail())
+                    .nickname(userDTO.getNickname())
+                    .password(userDTO.getPassword())
+                    .loginType(userDTO.getLoginType())
+                    .build();
+
+            UserEntity responseUser = userService.createUser(userEntity);
+
+            UserDTO responseUserDTO = UserDTO.builder()
+                    .email(responseUser.getEmail())
+                    .nickname(responseUser.getNickname())
+                    .password(responseUser.getPassword())
+                    .loginType(responseUser.getLoginType())
+                    .profileImage(responseUser.getProfileImage())
+                    .userId(responseUser.getUserId())
+                    .build();
+            return ResponseEntity.ok().body(responseUserDTO);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 }
