01097458f8acc66d2e717b43b2fb076f8151a6c1
명길식 <myeong-gilsig@MBook.local>
2024-02-16 18:42:11 +0900

    Fix: UserController, TokenProvider 코드 일부 수정

--- server/src/main/java/studysns/server/controller/UserController.java
+++ server/src/main/java/studysns/server/controller/UserController.java
@@ -1,15 +1,15 @@
 package studysns.server.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/user")
 public class UserController {
 
     @GetMapping("/signup")
     public String getSignIn(){
-        return "signup";
+        return "GET: user";
     }
 }
