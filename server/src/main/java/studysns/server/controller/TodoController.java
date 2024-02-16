38c9362e06f46c49acf2017302af026012cee5b7
kjb990202 <kjb990202@gmail.com>
2024-02-16 17:54:09 +0900

    Feat : todo 컨트롤러, DTO 틀 작성

--- server/src/main/java/studysns/server/controller/TodoController.java
+++ server/src/main/java/studysns/server/controller/TodoController.java
@@ -1,4 +1,25 @@
 package studysns.server.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/todo")
 public class TodoController {
+
+    @GetMapping("/gettodo/{nickname,date}")
+    public String gettodo() {
+        return "gettodo";
+    }
+
+    @PostMapping("/maketodo")
+    public String maketodo() {
+        return "maketodo";
+    }
+
+    @DeleteMapping("/deletetodo/{todoId}")
+    public String deletetodo(@PathVariable long todoId) {
+        return "deletetodo";
+    }
+
 }
