ced7c55a337fbf95e39a46ed04471015fb12e7e5
kjb990202 <kjb990202@gmail.com>
2024-02-16 18:37:23 +0900

    Fix : gettodo 경로 수정

--- server/src/main/java/studysns/server/controller/TodoController.java
+++ server/src/main/java/studysns/server/controller/TodoController.java
@@ -2,18 +2,19 @@ package studysns.server.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import studysns.server.dto.TodoDTO;
 
 @Controller
 @RequestMapping("/todo")
 public class TodoController {
 
-    @GetMapping("/gettodo/{nickname,date}")
-    public String gettodo() {
+    @GetMapping("/gettodo/{nickname}/{date}")
+    public String gettodo(@PathVariable String nickname, @PathVariable String date) {
         return "gettodo";
     }
 
     @PostMapping("/maketodo")
-    public String maketodo() {
+    public String maketodo(@RequestBody TodoDTO todoDTO) {
         return "maketodo";
     }
 
