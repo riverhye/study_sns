746fb40c6489c3c89f869bf598a70173d3959c65
J-Yeonju <Yeonju.dev@gmail.com>
2024-02-17 17:48:38 +0900

    Style:닉네임, 아이디 수정

--- client/components/sign/SignIn.tsx
+++ client/components/sign/SignIn.tsx
@@ -45,7 +45,7 @@ export default function SignIn() {
                   </div>
 
                   <button type="submit" className="w-full bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-                    Sign in
+                    로그인
                   </button>
               </form>
 
