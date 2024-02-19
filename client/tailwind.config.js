062d5ee41b327ade7a162dc3b92bdc9c2fc890a7
riverhye <uhzigani@gmail.com>
2024-02-19 13:01:26 +0900

    Fix: tailwind purge -> content

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,6 +4,6 @@ module.exports = {
   theme: {
     extend: {},
   },
-  purge: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   plugins: [require('tailwindcss'), require('autoprefixer')],
 };
