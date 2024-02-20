28dadf7af556d4a5bb7c238e7aafa8bdb6d48b9b
riverhye <uhzigani@gmail.com>
2024-02-21 02:16:23 +0900

    Design: 피드 컴포넌트

--- client/components/feed/HomeFeed.tsx
+++ client/components/feed/HomeFeed.tsx
@@ -80,7 +80,10 @@ const HomeFeed = () => {
     <>
       <section>
         <div className="flex justify-center h-12 w-full">
-          <input className="w-1/2 outline-none rounded-md shadow-sm block indent-3 focus:outline-none focus:ring-sky-500 focus:ring-1 focus:border-sky-500  placeholder:text-slate-400" />
+          <input
+            placeholder="오늘 할 공부는?"
+            className="w-1/2 outline-none rounded-md shadow-sm block indent-3 focus:outline-none focus:ring-sky-500 focus:border-sky-500  placeholder:text-slate-400"
+          />
           <button onClick={startStudy} type="button" className="w-32 border-2 rounded-full">
             시작
           </button>
