ccb11da2ff6a71bfedab6f4fd6cde4aa78421cb6
riverhye <uhzigani@gmail.com>
2024-02-22 21:47:04 +0900

    Design: 팔로워 슬라이더 시도

---
+++ client/components/TempSlider.tsx
@@ -0,0 +1,32 @@
+// import { Swiper, SwiperSlide } from "swiper/react";
+
+// import "swiper/css";
+// import "swiper/css/effect-coverflow";
+// import "swiper/css/pagination";
+
+// import { EffectCoverflow, Pagination } from "swiper";
+
+// export default function TempSlider(){
+//     return <>
+//           <Swiper
+//         effect={"coverflow"}
+//         grabCursor={true}
+//         centeredSlides={true}
+//         slidesPerView={"auto"}
+//         coverflowEffect={{
+//           rotate: 50,
+//           stretch: 0,
+//           depth: 100,
+//           modifier: 1,
+//           slideShadows: true,
+//         }}
+//         pagination={true}
+//         modules={[EffectCoverflow, Pagination]}
+//         className="mySwiper"
+//       >
+//         <SwiperSlide>
+//           <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+//         </SwiperSlide>
+//       </Swiper>
+//     </>
+// }
--- client/components/header/HeaderTop.tsx
+++ client/components/header/HeaderTop.tsx
@@ -43,45 +43,35 @@ const HeaderTop: React.FC = () => {
   const swiperRef = useRef<SwiperCore | null>(null);
 
   return (
-    // <Swiper
-    //   centeredSlides={true} //가운데 정렬
-    //   slidesPerView={1} //한 슬라이드에 보여줄 갯수
-    //   spaceBetween={20} //슬라이드간 거리
-    //   navigation // 이동 화살표
-    //   modules={[Navigation]}
-    //   breakpoints={{
-    //     768: {
-    //       slidesPerView: 5, // 768px 이상일 때 5개 보이게 설정
-    //     },
-    //     1024: {
-    //       slidesPerView: 10, // 1024px 이상일 때 10개 보이게 설정
-    //     },
-    //   }}>
-    <div className="w-full h-32 p-4 flex items-center">
-      {followerList.map((follower, index) => (
-        // <SwiperSlide>
-        <div key={index} className="flex flex-col items-center mx-4 relative">
-          {index < 3 && (
-            <div className="absolute left-0 top-0 md:left-0 text-xl text-amber-400 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
-              {index + 1}
-            </div>
-          )}
-          <div className="has-tooltip">
-            <Link href={`/`}>
-              <div className="border-x-strong-yellow border-y-subtle-yellow	border-2 rounded-full w-20 h-20 overflow-hidden">
-                <Image src="/blank-profile.png" alt="user profile" width={300} height={300} />
+    <Swiper
+      slidesPerView={5} // 한 번에 보여질 아이템의 수
+      slidesPerGroup={5} // 한 번에 슬라이드할 그룹의 수
+      navigation>
+      <div className="w-full h-32 p-4 flex items-center">
+        {followerList.map((follower, index) => (
+          <SwiperSlide>
+            <div key={index} className="flex flex-col items-center mx-4 relative">
+              {index < 3 && (
+                <div className="absolute left-0 top-0 md:left-0 text-xl text-amber-400 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+                  {index + 1}
+                </div>
+              )}
+              <div className="has-tooltip">
+                <Link href={`/`}>
+                  <div className="border-x-strong-yellow border-y-subtle-yellow	border-2 rounded-full w-20 h-20 overflow-hidden">
+                    <Image src="/blank-profile.png" alt="user profile" width={300} height={300} />
+                  </div>
+                </Link>
+                <div className="tooltip text-sm rounded shadow-lg p-1 w-24 bg-white text-main-blue mt-3 text-center">
+                  {follower.nickname}
+                  <span className="block text-center">00:00</span>
+                </div>
               </div>
-            </Link>
-            <div className="tooltip text-sm rounded shadow-lg p-1 w-24 bg-white text-main-blue mt-3 text-center">
-              {follower.nickname}
-              <span className="block text-center">00:00</span>
             </div>
-          </div>
-        </div>
-        // </SwiperSlide>
-      ))}
-    </div>
-    // </Swiper>
+          </SwiperSlide>
+        ))}
+      </div>
+    </Swiper>
   );
 };
 
