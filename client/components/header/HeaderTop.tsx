adf57099f2172a1d2a57748cbebfca871a926cb4
riverhye <uhzigani@gmail.com>
2024-02-24 02:28:36 +0900

    Fix: swiper.js 문제

--- client/components/header/HeaderTop.tsx
+++ client/components/header/HeaderTop.tsx
@@ -2,16 +2,14 @@
 
 import axios from 'axios';
 import Link from 'next/link';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 
-import SwiperCore from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
-import Image from 'next/image';
 
-SwiperCore.use([Navigation]);
+import Image from 'next/image';
 
 const HeaderTop: React.FC = () => {
   const [followerList, setFollowerList] = useState([
@@ -40,38 +38,62 @@ const HeaderTop: React.FC = () => {
     // getData();
   }, [followerList]);
 
-  const swiperRef = useRef<SwiperCore | null>(null);
-
   return (
-    <Swiper
-      slidesPerView={5} // 한 번에 보여질 아이템의 수
-      slidesPerGroup={5} // 한 번에 슬라이드할 그룹의 수
-      navigation>
-      <div className="w-full h-32 p-4 flex items-center">
-        {followerList.map((follower, index) => (
-          <SwiperSlide>
-            <div key={index} className="flex flex-col items-center mx-4 relative">
-              {index < 3 && (
-                <div className="absolute left-0 top-0 md:left-0 text-xl text-amber-400 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
-                  {index + 1}
-                </div>
-              )}
-              <div className="has-tooltip">
-                <Link href={`/`}>
-                  <div className="border-x-strong-yellow border-y-subtle-yellow	border-2 rounded-full w-20 h-20 overflow-hidden">
-                    <Image src="/blank-profile.png" alt="user profile" width={300} height={300} />
+    <div className="mt-5">
+      <Swiper
+        modules={[Navigation]}
+        spaceBetween={20}
+        slidesPerView={10}
+        slidesPerGroup={5}
+        navigation
+        breakpoints={{
+          320: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          480: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+          },
+          640: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+          },
+          768: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 5,
+            spaceBetween: 50,
+          },
+        }}>
+        <div className="w-full h-32 p-4 flex items-center">
+          {followerList.map((follower, index) => (
+            <SwiperSlide>
+              <div key={index} className="flex flex-col items-center mx-4 relative">
+                {index < 3 && (
+                  <div className="absolute left-10 top-0 md:left-10 text-2xl text-amber-400 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+                    {index + 1}
                   </div>
-                </Link>
-                <div className="tooltip text-sm rounded shadow-lg p-1 w-24 bg-white text-main-blue mt-3 text-center">
-                  {follower.nickname}
-                  <span className="block text-center">00:00</span>
+                )}
+                <div className="has-tooltip">
+                  <Link href={`/`}>
+                    <div className="border-x-strong-yellow border-y-subtle-yellow	border-2 rounded-full w-20 h-20 overflow-hidden">
+                      <Image src="/blank-profile.png" priority={false} alt="user profile" width={300} height={300} />
+                    </div>
+                  </Link>
+                  <div className=" absolute top-4 tooltip text-sm rounded shadow-lg p-1 w-24 bg-white text-main-blue mt-3 text-center cursor-default">
+                    {follower.nickname}
+                    <span className="block text-center cursor-default">00:00</span>
+                  </div>
                 </div>
               </div>
-            </div>
-          </SwiperSlide>
-        ))}
-      </div>
-    </Swiper>
+            </SwiperSlide>
+          ))}
+        </div>
+      </Swiper>
+    </div>
   );
 };
 
