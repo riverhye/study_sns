04e4feff99c0d02272dd30d8ef4b939ed04c3179
J-Yeonju <Yeonju.dev@gmail.com>
2024-02-24 18:09:34 +0900

    feat: 닉네임 placeholder에 삽입

--- client/components/sign/EditProfile.tsx
+++ client/components/sign/EditProfile.tsx
@@ -4,30 +4,28 @@ import { useState } from 'react';
 import axios from 'axios';
 
 const EditProfile = () => {
+  const placeHolderNickname: string = localStorage.getItem("nickname") ?? '';
   const [email, setEmail] = useState('');
   const [nickname, setNickname] = useState('');
   const [currentPassword, setCurrentPassword] = useState('');
   const [newPassword, setNewPassword] = useState('');
   const [confirmNewPassword, setConfirmNewPassword] = useState('');
 
-  const handleUpdateProfile = async (e) => {
+  const handleUpdateProfile = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
-      // Perform password validation before submitting the form
       if (newPassword !== confirmNewPassword) {
         alert('새 비밀번호가 일치하지 않습니다.');
         return;
       }
 
       const res = await axios.put(`${process.env.NEXT_PUBLIC_URL}/user/update`, {
-        email: email,
         nickname: nickname,
         currentPassword: currentPassword,
         newPassword: newPassword
       });
 
       alert('정보 수정이 완료되었습니다.');
-      // Optional: Redirect to a profile page or other appropriate route
     } catch (error) {
       console.error('Error updating profile:', error);
       alert('정보 수정 실패');
@@ -45,22 +43,11 @@ const EditProfile = () => {
               </h1>
               <form className="space-y-4 md:space-y-6" onSubmit={handleUpdateProfile}>
                 <div>
-                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">이메일</label>
-                  <input
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-60 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    type="email"
-                    placeholder="Email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                  <button type="button">수정</button>
-                </div>
-                <div>
                   <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">닉네임</label>
                   <input
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-60 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     type="text"
-                    placeholder="Nickname"
+                    placeholder={placeHolderNickname}
                     value={nickname}
                     onChange={(e) => setNickname(e.target.value)}
                   />
@@ -99,6 +86,11 @@ const EditProfile = () => {
                 <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                   수정하기
                 </button>
+
+                <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                  탈퇴하기
+                </button>
+
               </form>
             </div>
           </div>
