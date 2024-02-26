9332714908f5ed52dd3c06802a98396e65a0769c
riverhye <uhzigani@gmail.com>
2024-02-27 01:49:19 +0900

    Feat: 명세서 맞춰서 타입 변경, 소켓 코드 작성

--- client/components/notification/Notify.tsx
+++ client/components/notification/Notify.tsx
@@ -4,37 +4,32 @@ import { UserFeedData } from '@/type/type';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import FeedContent from '../feed/FeedContent';
+import { useWebSocket } from '../providers/SocketContext';
 
 export const token = localStorage.getItem('accessToken');
 
 const Notify = () => {
   const initialFeedData: UserFeedData[] = [
     {
-      feedId: 62,
+      feedId: 92,
       nickname: '테스트',
-      image: 'image',
-      content: '작성 내용',
-      type: '시작했습니다.',
-      date: new Date(),
-      isLike: true,
+      profileImage: null,
+      message: 'ㅁㅁ님이 나를 팔로우합니다.',
+      date: String(new Date()),
     },
     {
-      feedId: 65,
-      nickname: '어쩌고',
-      content: '리액트',
-      type: '시작했습니다.',
-      image: 'image',
-      date: new Date('2024-02-19T12:34:56'),
-      isLike: true,
+      feedId: 169,
+      nickname: '티니',
+      profileImage: null,
+      message: '나나님이 나의 피드에 좋아요를 눌렀습니다.',
+      date: '2024-02-24',
     },
     {
-      feedId: 67,
-      nickname: '맞는데요',
-      image: 'image2',
-      content: '게임',
-      type: '마쳤습니다.',
-      date: new Date('2024-02-14T12:34:56'),
-      isLike: false,
+      feedId: 203,
+      nickname: '미역국',
+      profileImage: null,
+      message: '호지차님이 나를 팔로우합니다.',
+      date: String(new Date()),
     },
   ];
   const [feedList, setFeedList] = useState<UserFeedData[]>(initialFeedData);
