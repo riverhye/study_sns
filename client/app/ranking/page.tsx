a033de4cee38fb33f94d2cbfd6f107e90feba3c9
zyam1 <zyam8441@gmail.com>
2024-02-26 16:21:10 +0900

    feat:랭킹페이지 프론트 완료

---
+++ client/app/ranking/page.tsx
@@ -0,0 +1,35 @@
+'use client';
+import RankUI from '@/components/ranking/RankUI';
+import axios from 'axios';
+import Link from 'next/link';
+import React, { useState } from 'react';
+interface RankingData {
+  profileImage: string;
+  nickname: string;
+  studyDate: string;
+  todoayStudyTime: number;
+}
+const Ranking = () => {
+  const [rankingList, getRankingList] = useState<RankingData[]>([
+    { profileImage: '', nickname: 'test', studyDate: '', todoayStudyTime: 150 },
+    { profileImage: '', nickname: '', studyDate: '', todoayStudyTime: 150 },
+  ]);
+  async function getRnaking() {
+    const res: RankingData[] = await axios.get(`${process.env.NEXT_PUBLIC_URL}/ranking`);
+
+    getRankingList(res);
+  }
+  return (
+    <div>
+      {rankingList.map((list, index) => (
+        <Link href={`study/${list.nickname}`}>
+          <div key={index} className=" mt-5">
+            <RankUI uidata={list} index={index} />
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default Ranking;
