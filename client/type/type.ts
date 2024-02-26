5053085ba9e4a5a5356a862b612728f954494db4
riverhye <uhzigani@gmail.com>
2024-02-27 01:44:54 +0900

    Feat & Design: Feed - API 맞춰서 type 변경, socket 코드 일부 추가

--- client/type/type.ts
+++ client/type/type.ts
@@ -17,22 +17,22 @@ export interface NicknamePropsType {
   //추가 가능성있음
 }
 
-// [피드] 모든 팔로워 정보
+// [홈] 피드
 export interface UserFeedData {
+  action?: string;
+  message: string;
+  date: string;
   feedId: number;
   nickname: string;
-  image: string;
-  content: string;
-  type: string;
-  date: Date;
+  profileImage: string | null;
   isLike?: boolean;
 }
 [];
 
-// [피드] 모든 팔로워 정보 props
+// [홈] 모든 팔로워 정보 props
 export interface UserFeedProps {}
 
-// [피드] 공통 부분 모듈화
+// [홈/알림] 피드 공통 부분 모듈화한 컴포넌트
 export interface FeedItemProps {
   feed: UserFeedData;
   children?: ReactNode;
@@ -40,45 +40,53 @@ export interface FeedItemProps {
   handleLike?: (index: number) => void;
 }
 
-// [피드] props
+// [홈/알림] FeedItem의 상위 컴포넌트 props
 export interface FeedContentProps {
   feedData?: UserFeedData[];
-  type?: string;
+  type?: string; // 좋아요 유무
   initialFeedData: UserFeedData[]; // temp
   handleLike?: (index: number) => void;
 }
 
-// 타이머
-export interface TimerState {
-  studyStatus?: string;
-  startPoint?: number;
-  savedStudyTime?: number;
-}
-
+// [홈] 공부 내용 input - error msg
 export interface StateValue {
   content: string;
   error: string;
 }
 
-// [Nav] Left 요청 받아온 데이터
-export interface HeaderLeftData {
-  content: string[];
-  // timer fields
-}
-
-// [Nav] 카테고리
+// [Nav-Left] 카테고리
 export interface Categories {
   href: string;
   text: string;
   svgComponent: React.ReactNode;
 }
 
-// [Nav] 투두
+// [Nav-Left] 리덕스용 타이머
+export interface TimerState {
+  studyStatus?: string;
+  startPoint?: number;
+  savedStudyTime?: number;
+}
+
+// [Nav-Left] 투두
 export interface TodoContent {
   todoId: number;
   todoContent: string;
   todoDate: string;
   nickname: string;
   userId: number;
+}
+[];
+
+// [Nav-Left] 타이머 아이콘 props
+export interface TimerIconProps {
+  onClick?: () => void;
+}
+
+// [Nav-Top] 팔로워 시간 랭킹
+export interface FollowerRank {
+  nickname: string;
+  profileImage: string | null;
+  todayStudyTime: number;
 }
 [];
