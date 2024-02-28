4935db09f6618dceb7e047a634b19acad8ff6196
blee94 <135qudwls@naver.com>
2024-02-29 02:12:41 +0900

    FIX: conflict

--- client/components/hooks/useTimerFunc.ts
+++ client/components/hooks/useTimerFunc.ts
@@ -83,13 +83,6 @@ const useTimerFunc = () => {
       } catch (error) {
         console.error('rank timer', error);
       }
-      socket.onmessage = (evt: IMessageEvent) => {
-        console.log(JSON.parse(evt.data as string));
-
-        // if(nickname === 받아온닉네임) {
-        // setFeedData(evt.data); // feedData에 넣기
-        // }
-      };
     }
 
     // const sendData = async () => {
